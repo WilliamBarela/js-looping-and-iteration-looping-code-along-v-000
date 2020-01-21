d5eefaf5515d2085c7bdbf16efcd5c4ef22d8e32
William Barela <william.barela.dev@gmail.com>
2020-01-21 20:52:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solutions in this file
+
+function writeCards(recipients, occasion){
+  const messages = [];
+  
+  for(let i = 0; i < recipients.length; i++){
+    messages.push(`Thank you, ${recipient}, for the wonderful ${occasion} card!`);
+  }
+  
+  return messages;
+}
